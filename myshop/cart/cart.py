e1fa364aa15d65346ac8c0a4cf8c5d9f0dac9c0c
marosan28 <danijela.marosan36@gmail.com>
2022-12-12 19:50:20 +0000

    sum the total price with calculated values

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -41,3 +41,6 @@ class Cart:
 
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
+        
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
