1419a9e40ea61a0fef1f8b6828ca500e5475796c
marosan28 <danijela.marosan36@gmail.com>
2022-12-12 11:11:29 +0000

    create a view to list all products

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
