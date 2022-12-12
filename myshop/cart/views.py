45f481e76c842d3c313ca0e7cf6c6b0d0dff1d24
marosan28 <danijela.marosan36@gmail.com>
2022-12-12 20:07:31 +0000

    add return function

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -21,3 +21,4 @@ def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
+    return redirect('cart:cart_detail')
