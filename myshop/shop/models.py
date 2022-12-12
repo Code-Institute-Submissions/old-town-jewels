9db36fca162ba602567237e555671cf07b70ec3d
marosan28 <danijela.marosan36@gmail.com>
2022-12-12 10:53:39 +0000

    add product model

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Category models
+# Category model
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -16,5 +16,26 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+# Product model
 
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+        ]
+        def __str__(self):
+            return self.name
 
