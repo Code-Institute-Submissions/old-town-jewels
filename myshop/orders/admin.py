ad9ea288d51b0dc84e83c3aca53cb58e5f16332b
marosan28 <danijela.marosan36@gmail.com>
2022-12-13 08:19:15 +0000

    add model to admin to include orders on page

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Order, OrderItem
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
