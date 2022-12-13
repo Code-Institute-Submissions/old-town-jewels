6008e6e1f2ed504fa3cdc22dccd9774b23d9d179
marosan28 <danijela.marosan36@gmail.com>
2022-12-13 08:14:49 +0000

    add model to store product, qty and price

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -15,3 +15,15 @@ class Order(models.Model):
         indexes = [
             models.Index(fields=['-created']),
         ]
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    def __str__(self):
+        return str(self.id)
