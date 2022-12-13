84a9c497ff6e1ae59bd478fafb8d206362da0937
marosan28 <danijela.marosan36@gmail.com>
2022-12-13 08:29:17 +0000

    add function to clear the cart

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -13,3 +13,12 @@ def order_create(request):
                                          product=item['product'],
                                          price=item['price'],
                                          quantity=item['quantity'])
+            cart.clear()
+            return render(request,
+                          'orders/order/created.html',
+                          {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request,
+                  'orders/order/create.html',
+                  {'cart': cart, 'form': form})
