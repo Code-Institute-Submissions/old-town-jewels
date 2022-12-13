592b697a65a8b73808dfade303ed3ded3994ed78
marosan28 <danijela.marosan36@gmail.com>
2022-12-13 15:03:36 +0000

    add delay after clearing the cart

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import order_created
+
 from cart.cart import Cart
 def order_create(request):
     cart = Cart(request)
@@ -14,6 +16,7 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
