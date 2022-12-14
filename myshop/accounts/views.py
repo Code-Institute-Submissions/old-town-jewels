e63473c3dc22cc69a36d6f22fc2c97569f761b3b
marosan28 <danijela.marosan36@gmail.com>
2022-12-14 21:15:59 +0000

    make function for registering the user

--- myshop/accounts/views.py
+++ myshop/accounts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 
 def register(request):
+
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -9,5 +12,24 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
+
+        if password1 == password2:
+            if User.objects.filter(username = username).exists():
+                messages.info(request, "Username Taken")
+                return redirect('register')
+            elif User.objects.filter(email = email).exists():
+                messages.info(request, "Email Taken")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                username = username, password = password1, email = email,
+                last_name = last_name, first_name = first_name)
+                user.save()
+                print("user created")
+        else:
+            print("password not matching")
+            return redirect('register')
+        return redirect(' / ')
+
     else:
         return render(request, 'register.html')
