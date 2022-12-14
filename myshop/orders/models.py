e102cdf46f1cb02aebaeccf65ac8508aa0511b23
marosan28 <danijela.marosan36@gmail.com>
2022-12-14 15:21:28 +0000

    no stripe id fix - add function to models to retrive stripe id

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -12,11 +12,30 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+
+
     class Meta:
         ordering = ['-created']
         indexes = [
             models.Index(fields=['-created']),
         ]
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            path = '/test/'
+        else:
+            path = '/'
+        return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
+
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,
                               related_name='items',
@@ -27,7 +46,10 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=10,
                                 decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
+
+
     def __str__(self):
         return str(self.id)
+        
     def get_cost(self):
         return self.price * self.quantity
