bbdbd6c2afdd3dd6ad41ed7d789e09225a085a0b
marosan28 <danijela.marosan36@gmail.com>
2022-12-14 13:46:15 +0000

    create urls for payment completed and canceled

--- myshop/payment/urls.py
+++ myshop/payment/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name = 'payment'
 urlpatterns = [
     path('process/', views.payment_process, name='process'),
+    path('completed/', views.payment_completed, name='completed'),
+    path('canceled/', views.payment_canceled, name='canceled'),
 ]
