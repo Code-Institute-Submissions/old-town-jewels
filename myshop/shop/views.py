4814a9e861de0dd1e967d5393d42b2477b3cc31a
marosan28 <danijela.marosan36@gmail.com>
2022-12-14 17:59:04 +0000

    create function for register in views

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login
+from django.contrib import messages
 from cart.forms import CartAddProductForm
+from .models import Category, Product
+from .forms import NewUserForm
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("main:homepage")
+        messages.error(
+            request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="main/register.html", context={"register_form": form})
 
 
 def product_list(request, category_slug=None):
@@ -22,4 +39,4 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/detail.html', {'product': product,
-                   'cart_product_form': cart_product_form})
+                                                        'cart_product_form': cart_product_form})
