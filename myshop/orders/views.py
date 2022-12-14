0df38cfc3f8c876d41b492aab6864d0166b8c9e8
marosan28 <danijela.marosan36@gmail.com>
2022-12-14 13:25:45 +0000

    set API key for stripe in views file

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -18,9 +18,9 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             order_created.delay(order.id)
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+            request.session['order_id'] = order.id
+            
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request,
--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from decimal import Decimal
+import stripe
+from django.conf import settings
+from django.shortcuts import render, redirect, reverse,\ 
+                            get_object_or_404
+from orders.models import Order
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_version = settings.STRIPE_API_VERSION
