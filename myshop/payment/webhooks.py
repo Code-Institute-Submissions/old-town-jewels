953cea246229bedf3c92115aa29d17ad60399768
marosan28 <danijela.marosan36@gmail.com>
2022-12-14 14:13:24 +0000

    create if loop to check if event received is completed

--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -15,9 +15,16 @@ def stripe_webhook(request):
                     sig_header,
                     settings.STRIPE_WEBHOOK_SECRET)
     except ValueError as e:
-        # Invalid payload
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
         return HttpResponse(status=400)
+     if event.type == 'checkout.session.completed':
+        session = event.data.object
+        if session.mode == 'payment' and session.payment_status == 'paid':
+            try:
+                order = Order.objects.get(id=session.client_reference_id)
+            except Order.DoesNotExist:
+                return HttpResponse(status=404)
+            order.paid = True
+            order.save()
     return HttpResponse(status=200)
