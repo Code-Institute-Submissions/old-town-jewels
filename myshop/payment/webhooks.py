a1082ad3bd8ad82daae54e03b8df7a9f04b47474
marosan28 <danijela.marosan36@gmail.com>
2022-12-14 14:10:32 +0000

    create file for weebhooks

---
+++ myshop/payment/webhooks.py
@@ -0,0 +1,7 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from orders.models import Order
+
+@csrf_exempt
