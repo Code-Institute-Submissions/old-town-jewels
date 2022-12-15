0832d06a9245e025308816a9160df99aff69b4cb
marosan28 <danijela.marosan36@gmail.com>
2022-12-15 08:11:15 +0000

    add messages to register form html

--- myshop/accounts/views.py
+++ myshop/accounts/views.py
@@ -14,25 +14,25 @@ def register(request):
 
         if password1 == password2:
             if User.objects.filter(username = username).exists():
-                messages.info(request, "Username Taken")
+                messages.add_message(request, messages.ERROR, "Username Taken")
                 return redirect('register')
             elif User.objects.filter(email = email).exists():
-                messages.info(request, "Email Taken")
+                messages.add_message(request, messages.ERROR, "Email Taken")
                 return redirect('register')
             else:
                 user = User.objects.create_user(
                 username = username, password = password1, email = email,
                 last_name = last_name, first_name = first_name)
                 user.save()
-                print("user created")
+                messages.add_message(request, messages.SUCCESS, "User created successfully")
         else:
-            print("password not matching")
+            messages.add_message(request, messages.ERROR, "Password not matching")
             return redirect('register')
         return redirect('/')
 
     else:
         if User.objects.filter(username = request.GET.get('username')).exists() or User.objects.filter(email = request.GET.get('email')).exists():
-            messages.info(request, "Username or Email Taken")
+            messages.add_message(request, messages.ERROR, "Username or Email Taken")
         context = {
             'request':request,
         }
